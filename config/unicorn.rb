b7a839b8a120c0c91b31be267fd7490e97ab9229
Faruk Celik <farukca@yahoo.com>
2012-04-16 18:07:44 +0300

    unicorn folder problem

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/freighter/apps/socialfreight/current"
+root = "/home/freighter/apps/SocialFreight/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.socialfreight.sock"
+listen "/tmp/unicorn.SocialFreight.sock"
 worker_processes 2
 timeout 30
