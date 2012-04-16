331b4565f3c85bb6a1d51bb1bdf1edf503bc4cef
Faruk Celik <farukca@yahoo.com>
2012-04-16 17:05:35 +0300

    unicorn user error

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -15,7 +15,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/freighter/apps/socialfreight/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=deployer
+AS_USER=freighter
 set -u
 
 OLD_PIN="$PID.oldbin"
