5b104eebb36c611d943378a0becbb02feea55cea
Faruk Celik <farukca@yahoo.com>
2012-04-20 23:52:41 +0300

    actionmailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,4 +62,15 @@ Socialfreight::Application.configure do
   # Rack::Cache [GET /] miss error
   config.action_dispatch.rack_cache[:verbose] = false
 
+  # Gmail settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'farukca2000@gmail.com',
+    :password             => '66210000',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+
 end
