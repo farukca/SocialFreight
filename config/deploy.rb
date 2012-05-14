73d4f2ed2b057b12e61e114482547f1bb8c3415b
Faruk Celik <farukca@yahoo.com>
2012-05-14 23:10:12 +0300

    uploads folder changes on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,14 +23,14 @@ ssh_options[:forward_agent] = true
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
- namespace :deploy do
-   %w[start stop deploy].each do |cmd|
-     desc "#{cmd} unicorn server"
-     task cmd, roles: :app, except: { no_release: true } do
-       run "/etc/init.d/unicorn_#{application} #{cmd}"
+namespace :deploy do
+  %w[start stop deploy].each do |cmd|
+    desc "#{cmd} unicorn server"
+    task cmd, roles: :app, except: { no_release: true } do
+      run "/etc/init.d/unicorn_#{application} #{cmd}"
       #run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-     end
-   end
+    end
+  end
 
   task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
@@ -65,4 +65,34 @@ ssh_options[:forward_agent] = true
       end
     end
   end
+
+  namespace :uploads do
+    desc <<-EOD
+      Creates the upload folders unless they exist and sets the proper upload permissions.
+    EOD
+    task :setup, :except => { :no_release => true } do
+      dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
+      run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+    end
+
+    desc <<-EOD
+      [internal] Creates the symlink to uploads shared folder for the most recently deployed version.
+    EOD
+    task :symlink, :except => { :no_release => true } do
+      run "rm -rf #{release_path}/public/uploads"
+      run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
+    end
+
+    desc <<-EOD
+      [internal] Computes uploads directory paths and registers them in Capistrano environment.
+    EOD
+    task :register_dirs do
+      set :uploads_dirs,    %w(uploads uploads/user uploads/patron uploads/person)
+      set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
+    end
+
+    after "deploy:finalize_update", "uploads:symlink"
+    on :start, "uploads:register_dirs"
+  end
+
 end
