c9b85f6767603c926f92f1d76ec5bf0627056138
Faruk Celik <farukca@yahoo.com>
2012-11-12 23:48:34 +0200

    zero downtime deployment configurations

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,3 +7,27 @@ stdout_path "#{root}/log/unicorn.log"
 listen "/tmp/unicorn.SocialFreight.sock"
 worker_processes 2
 timeout 30
+
+before_fork do |server, worker|
+  # Disconnect since the database connection will not carry over
+  if defined? ActiveRecord::Base
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Quit the old unicorn process
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  # Start up the database connection again in the worker
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
