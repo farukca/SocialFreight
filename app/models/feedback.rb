c288aaa2000e95d1eb38960a2979760f203c3ebd
Faruk Celik <farukca@yahoo.com>
2012-11-16 09:21:28 +0200

    feedback validation messages removed

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,9 +2,9 @@ class Feedback < ActiveRecord::Base
 
   belongs_to :patron
   
-  validates_presence_of :name, :email, :msg, :message => "Lütfen bilgi giriniz"
-  validates_length_of :name, maximum: 40, :message => "10 karakter giriniz"
-  validates_length_of :email, maximum: 40, :message => "10 karakter giriniz"
+  validates_presence_of :name, :email, :msg
+  validates_length_of :name, maximum: 40
+  validates_length_of :email, maximum: 40
   validates_length_of :msg, maximum: 500
 
 end
