3f3f3033c583c281a094c51cd0a1343da086a01d
Faruk Celik <farukca@yahoo.com>
2012-12-12 00:15:31 +0200

    web side locale changes

--- db/migrate/20120908203248_add_load_place_to_departures.rb
+++ db/migrate/20120908203248_add_load_place_to_departures.rb
@@ -1,6 +1,7 @@
 class AddLoadPlaceToDepartures < ActiveRecord::Migration
   def change
   	change_table :departures do |t|
+  	  t.boolean  :pre_trans, default: false
   	  t.string   :load_place, limit: 60
   	end
   end
