adc8d32a39753ac8c0e4ab964e5fda4e33616c51
Faruk Celik <farukca@yahoo.com>
2013-07-20 13:26:59 +0300

    Local server deployment changes

--- config/recipes/rbenv.rb
+++ config/recipes/rbenv.rb
@@ -18,13 +18,10 @@ BASHRC
     run "mv ~/.bashrc.tmp ~/.bashrc"
     run %q{export PATH="$HOME/.rbenv/bin:$PATH"}
     run %q{eval "$(rbenv init -)"}
-    #run "rbenv #{rbenv_bootstrap}"
-    #run %q{sed "s/sudo/sudo -p 'sudo password: '/g" $HOME/.rbenv/plugins/rbenv-installer/bin/rbenv-} + rbenv_bootstrap + " | bash"
-    #run "rbenv install #{ruby_version}"
-    #run "rbenv global #{ruby_version}"
-    runrbenv "#{rbenv_bootstrap}"
-    runrbenv "install #{ruby_version}"
-    runrbenv "global #{ruby_version}"
+    run "rbenv #{rbenv_bootstrap}"
+    run %q{sed "s/sudo/sudo -p 'sudo password: '/g" $HOME/.rbenv/plugins/rbenv-installer/bin/rbenv-} + rbenv_bootstrap + " | bash"
+    run "rbenv install #{ruby_version}"
+    run "rbenv global #{ruby_version}"
     run "gem install bundler --no-ri --no-rdoc"
     runrbenv "rehash"
   end
