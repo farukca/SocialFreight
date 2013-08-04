954eabbc5d36539af94f1d285235650dccacb01b
Faruk Celik <farukca@yahoo.com>
2013-08-04 04:09:06 +0300

    add hstore extension to migration

--- db/migrate/20130723091757_add_filter_to_searches.rb
+++ db/migrate/20130723091757_add_filter_to_searches.rb
@@ -1,3 +1,11 @@
+class SetupHstore < ActiveRecord::Migration
+  def self.up
+    enable_extension "hstore"
+  end
+  def self.down
+    disable_extension "hstore"
+  end
+end
 class AddFilterToSearches < ActiveRecord::Migration
   def change
     add_column :searches, :filter, :hstore
