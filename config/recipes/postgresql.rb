b1726f934a9317d067e6ee46d896e78d45d77225
Faruk Celik <farukca@yahoo.com>
2013-08-25 23:42:52 +0300

    Reference added to tickets

--- config/recipes/postgresql.rb
+++ config/recipes/postgresql.rb
@@ -17,6 +17,7 @@ namespace :postgresql do
   task :create_database, roles: :db, only: {primary: true} do
     run %Q{#{sudo} -u postgres psql -c "create user #{postgresql_user} with password '#{postgresql_password}';"}
     run %Q{#{sudo} -u postgres psql -c "create database #{postgresql_database} owner #{postgresql_user};"}
+    #run %Q{#{sudo} -u postgres psql -c "create EXTENSION IF NOT EXISTS hstore WITH SCHEMA public;"}
   end
   after "deploy:setup", "postgresql:create_database"
 
