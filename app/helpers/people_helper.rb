3c481f5ddf8bfec50d397c45ee75c4c881f3b637
Faruk Celik <farukca@yahoo.com>
2013-08-30 14:54:47 +0300

    nimbos changes for ulusoy branch 2

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -4,13 +4,8 @@ module PeopleHelper
   	person.to_s
   end
 
-<<<<<<< HEAD
   def person_small_avatar(person = @person)
     cl_image_tag(person.avatar_url, width: 48, height: 48, crop: :fill, class: "media-object img-circle")
-=======
-  def person_avatar_name(person=@person)
-  	user_mini_avatar(person) + person.to_s
->>>>>>> origin/ulusoy
   end
 
 end
